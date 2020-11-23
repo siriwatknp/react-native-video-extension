94c93317d5556ce79295c38a03ecfe2f10655e75
siriwatknp <siriwatkunaporn@gmail.com>
2020-11-23 21:33:26 +0700

    wip: fullscreen state

--- lib/VideoPlus.tsx
+++ lib/VideoPlus.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { StyleSheet, View, ViewProps } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, ViewProps, useWindowDimensions } from 'react-native';
 import Video, { VideoProperties } from 'react-native-video';
 
-export interface VideoPlusProps extends VideoProperties {
+export interface VideoPlayerProps extends VideoProperties {
   style?: ViewProps['style'];
   videoStyle?: VideoProperties['style'];
 }
 
-const VideoPlus = ({ style, videoStyle, ...props }: VideoPlusProps) => {
+const VideoPlayer = ({ style, videoStyle, ...props }: VideoPlayerProps) => {
+  const { width, height } = useWindowDimensions();
+  const [fullscreen, setFullscreen] = useState(false);
+  const fullscreenStyle = {
+    view: {
+      position: 'absolute' as const,
+      width: height,
+      height: width,
+      top: 0,
+      left: 0,
+      transform: [
+        { translateY: (height - width) / 2 },
+        { translateX: -(height - width) / 2 },
+        { rotate: '90deg' },
+      ],
+    },
+    video: StyleSheet.absoluteFill,
+  };
   return (
-    <View style={[styles.container, style]}>
-      <Video style={[styles.video, videoStyle]} {...props} />
+    <View style={[fullscreen ? fullscreenStyle.view : styles.container, style]}>
+      <Video
+        style={[fullscreen ? fullscreenStyle.video : styles.video, videoStyle]}
+        {...props}
+      />
     </View>
   );
 };
@@ -22,7 +42,8 @@ const styles = StyleSheet.create({
   video: {
     height: undefined,
     aspectRatio: 16 / 9,
+    maxWidth: '100%',
   },
 });
 
-export default VideoPlus;
+export default VideoPlayer;
